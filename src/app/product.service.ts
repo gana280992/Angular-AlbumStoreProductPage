5181bb97e68753def094b627d3ec1642b793db68
Ganapathi Chinnakaruppan <gana280992@gmail.com>
2021-11-05 19:37:41 +0000

    Comiting Part3

---
+++ src/app/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ProductService {
+  constructor(private _http: Http) { }
+  private _albumUrl = '../assets/album.json';
+  getAlbum(id:number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response:Response) => response.json());
+  }
+}
+interface Album {
+  id: number,
+  artist: string,
+  album: AlbumDetails
+}
+interface AlbumDetails {
+  name: string,
+  releaseDate: string,
+  coverImage: string,  
+}
