a0babf412860c100a8852c2a9017f60579a15082
Ganapathi Chinnakaruppan <gana280992@gmail.com>
2021-11-05 20:09:41 +0000

    Commit Part4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,22 +2,13 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
   constructor(private _http: Http) { }
   private _albumUrl = '../assets/album.json';
   getAlbum(id:number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response:Response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
-}
-interface Album {
-  id: number,
-  artist: string,
-  album: AlbumDetails
-}
-interface AlbumDetails {
-  name: string,
-  releaseDate: string,
-  coverImage: string,  
 }
